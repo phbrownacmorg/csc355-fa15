1e8d7afc430ea57c591b9180d486135ea4e29e4d
Peter Brown <peter.brown@converse.edu>
2015-11-25 21:18:48 -0500

    Updated CountDownTimer to add a stop() method.

--- CountDownTimer.js
+++ CountDownTimer.js
@@ -1,54 +1,63 @@
 // Code taken from http://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer.
 // Written by robbmj (http://stackoverflow.com/users/2126755/robbmj)
 // Downloaded by Peter Brown on 2015-11-24
+// Modified to allow the timer to be stopped.  (Restarting is not supported.)
+
 
 function CountDownTimer(duration, granularity) {
-  this.duration = duration;
-  this.granularity = granularity || 1000;
-  this.tickFtns = [];
-  this.running = false;
+    this.duration = duration;
+    this.granularity = granularity || 1000;
+    this.tickFtns = [];
+    this.timeoutID = undefined;
+    this.timeLeft = 0;
 }
 
 CountDownTimer.prototype.start = function() {
-  if (this.running) {
-    return;
-  }
-  this.running = true;
-  var start = Date.now(),
-      that = this,
-      diff, obj;
-
-  (function timer() {
-    diff = that.duration - (((Date.now() - start) / 1000) | 0);
-
-    if (diff > 0) {
-      setTimeout(timer, that.granularity);
-    } else {
-      diff = 0;
-      that.running = false;
+    if (this.timeoutID !== undefined) {
+	return;
     }
+    var start = Date.now(),
+	that = this,
+	obj;
 
-    obj = CountDownTimer.parse(diff);
-    that.tickFtns.forEach(function(ftn) {
-      ftn.call(this, obj.minutes, obj.seconds);
-    }, that);
-  }());
+    (function timer() {
+	that.timeLeft = that.duration - (((Date.now() - start) / 1000) | 0);
+
+	if (that.timeLeft > 0) {
+	    that.timeoutID = setTimeout(timer, that.granularity);
+	} else {
+	    that.timeLeft = 0;
+	    that.timeoutID = undefined;
+	}
+
+	obj = CountDownTimer.parse(that.timeLeft);
+	that.tickFtns.forEach(function(ftn) {
+	    ftn.call(this, obj.minutes, obj.seconds);
+	}, that);
+    }());
 };
 
 CountDownTimer.prototype.onTick = function(ftn) {
-  if (typeof ftn === 'function') {
-    this.tickFtns.push(ftn);
-  }
-  return this;
+    if (typeof ftn === 'function') {
+	this.tickFtns.push(ftn);
+    }
+    return this;
 };
 
 CountDownTimer.prototype.expired = function() {
-  return !this.running;
+    return this.timeLeft === 0;
+};
+
+CountDownTimer.prototype.stop = function() {
+    if (this.timeoutID !== undefined) {
+	clearTimeout(this.timeoutID);
+	this.timeoutID = undefined;
+    }
 };
 
 CountDownTimer.parse = function(seconds) {
-  return {
-    'minutes': (seconds / 60) | 0,
-    'seconds': (seconds % 60) | 0
-  };
+    return {
+	'minutes': (seconds / 60) | 0,
+	'seconds': (seconds % 60) | 0
+    };
 };
